b995dee3b218ea2c376d19c02c47f7618b4f8b72
aantle11 <alicia.antle@keyin.com>
2025-08-03 21:24:17 -0230

    WorkoutClass data and UserService complete

--- Database/WorkoutClassData.java
+++ Database/WorkoutClassData.java
@@ -1,5 +1,30 @@
 package Database;
 
+import java.util.List;
+import java.util.ArrayList;
+import Classes.WorkoutClass;
+
 public class WorkoutClassData {
-    
+    private List<WorkoutClass> classList = new ArrayList<>();
+
+    public void addWorkoutClass(WorkoutClass wc) {
+        classList.add(wc);
+    }
+
+    public WorkoutClass getWorkoutClassById(int id) {
+        for (WorkoutClass wc : classList) {
+            if (wc.getId() == id) {
+                return wc;
+            }
+        }
+        return null;
+    }
+
+    public List<WorkoutClass> getAllWorkoutClasses() {
+        return classList;
+    }
+
+    public void removeWorkoutClass(int id) {
+        classList.removeIf(wc -> wc.getId() == id);
+    }
 }
--- Services/UserService.java
+++ Services/UserService.java
@@ -1,5 +1,60 @@
 package Services;
 
+import Classes.User;
+import Classes.Member;
+import Database.UserData;
+
 public class UserService {
-    
+    private UserData userData;
+
+    public UserService(UserData userData) {
+        this.userData = userData;
+    }
+
+    public boolean registerUser(int id, String name, String password, String role) {
+        if (name == null || password == null || name.isEmpty() || password.isEmpty()) {
+            System.out.println("Name and password cannot be empty");
+                return false;
+    }
+
+    if (userData.findUserByUsername(name) != null) {
+        System.out.println("Username is already taken");
+            return false;
+    }
+
+    String email = name.toLowerCase() + "@email.com";
+
+    User newUser = new Member(id, name, email, password, "Standard", true);
+    userData.addUser(newUser);
+    System.out.println("Registered successfully");
+    return true;
+}
+
+public User loginUser(String name, String password) {
+    User user = userData.findUserByUsername(name);
+
+    if (user != null && user.getPassword().equals(password)) {
+        System.out.println("Welcome, " + user.getName() + "!");
+        return user;
+    }
+
+    System.out.println("invalid username or password");
+    return null;
+}
+
+ public boolean deleteUser(String name) {
+    User user = userData.findUserByUsername(name);
+    if (user != null) {
+        userData.removeUser(name);
+        System.out.println("User deleted");
+        return true;
+    }
+
+    System.out.println("User not found");
+    return false;
+ }
+
+ public User getUser(String name) {
+    return userData.findUserByUsername(name);
+ }
 }
