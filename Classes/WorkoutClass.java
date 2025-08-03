72a2c23bd2614011a0825ec39bcaab23ad49fce1
aantle11 <alicia.antle@keyin.com>
2025-08-03 03:19:49 -0230

    WorkoutClass class complete & UserData complete

--- Classes/WorkoutClass.java
+++ Classes/WorkoutClass.java
@@ -1,5 +1,26 @@
 package Classes;
 
 public class WorkoutClass {
-    
+    private int id;
+    private String name;
+    private String schedule;
+    private String trainerName;
+
+    public WorkoutClass(int id, String name, String schedule, String trainerName) {
+        this.id = id;
+        this.name = name;
+        this.schedule = schedule;
+        this.trainerName = trainerName;
+    }
+
+    // Getters & Setters
+    public int getId() {return id;}
+    public String getName() {return name;}
+    public String getSchedule() {return schedule;}
+    public String getTrainerName() {return trainerName;}
+
+    @Override
+    public String toString() {
+        return name + " with " + trainerName + " at " + schedule;
+    }
 }
--- Database/UserData.java
+++ Database/UserData.java
@@ -1,5 +1,26 @@
 package Database;
 
+import java.util.List;
+import java.util.ArrayList;
+import Classes.User;
+
 public class UserData {
-    
+    private List<User> users = new ArrayList<>();
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public User findUserByUsername(String username) {
+        for (User user : users) {
+            if (user.getName().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void removeUser(String username) {
+        users.removeIf(user -> user.getName().equals(username));
+    }
 }
